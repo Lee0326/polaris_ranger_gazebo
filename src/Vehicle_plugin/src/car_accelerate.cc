a698912fc049343739874e36a2f510d3afd9abb4
Lee0326 <lcyfly1@163.com>
2020-03-27 13:20:28 -0400

    add brake and handbrake joint

--- src/Vehicle_plugin/src/car_accelerate.cc
+++ src/Vehicle_plugin/src/car_accelerate.cc
@@ -14,14 +14,18 @@ public:
         //Store the pointer to the model
         this->model = _parent;
         std::string gasPedalJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("gas_pedal");
+        std::string handBrakeJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("hand_brake_pedal");
+        std::string brakeJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("brake_pedal");
         this->gasPedalJoint = this->model->GetJoint(gasPedalJointName);
-        if (!this->gasPedalJoint)
+        this->handBrakeJoint = this->model->GetJoint(handBrakeJointName);
+        this->brakeJoint = this->model->GetJoint(brakeJointName);
+        if (!this->gasPedalJoint || !this->handBrakeJoint || !this->brakeJoint)
         {
-            gzthrow("could not find gas pedal joint\n");
+            gzthrow("could not find neccessary pedal joint\n");
         }
         else
         {
-            std::cout << "found the pedal joint!"
+            std::cout << "found all the pedal joints!"
                       << "\n";
         }
 
@@ -38,12 +42,16 @@ public:
         // Apply a small linear velocity to the model.
         //this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
         this->gasPedalJoint->SetForce(0, 500000);
+        this->gasPedalJoint->SetForce(0, 0);
+        this->gasPedalJoint->SetForce(0, 0);
     }
 
     // Pointer to the model
 private:
     physics::ModelPtr model;
     physics::JointPtr gasPedalJoint;
+    physics::JointPtr handBrakeJoint;
+    physics::JointPtr brakeJoint;
 
     // Pointer to the update event connection
 private:
