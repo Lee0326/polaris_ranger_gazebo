dd84b4ea7621b141a965f3612a595939e50dc0db
Lee0326 <lcyfly1@163.com>
2020-03-28 09:01:45 -0400

    apply force to joints of wheels

--- src/Vehicle_plugin/src/car_accelerate.cc
+++ src/Vehicle_plugin/src/car_accelerate.cc
@@ -16,10 +16,20 @@ public:
         std::string gasPedalJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("gas_pedal");
         std::string handBrakeJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("hand_brake_pedal");
         std::string brakeJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("brake_pedal");
+        std::string flWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("front_left_wheel");
+        std::string frWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("front_right_wheel");
+        std::string blWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("back_left_wheel");
+        std::string brWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("back_right_wheel");
+
         this->gasPedalJoint = this->model->GetJoint(gasPedalJointName);
         this->handBrakeJoint = this->model->GetJoint(handBrakeJointName);
         this->brakeJoint = this->model->GetJoint(brakeJointName);
-        if (!this->gasPedalJoint || !this->handBrakeJoint || !this->brakeJoint)
+        this->flWheelJoint = this->model->GetJoint(flWheelJointName);
+        this->frWheelJoint = this->model->GetJoint(frWheelJointName);
+        this->blWheelJoint = this->model->GetJoint(blWheelJointName);
+        this->brWheelJoint = this->model->GetJoint(brWheelJointName);
+
+        if (!this->gasPedalJoint || !this->handBrakeJoint || !this->brakeJoint || !this->flWheelJoint || !this->frWheelJoint || !this->blWheelJoint || !this->brWheelJoint)
         {
             gzthrow("could not find neccessary pedal joint\n");
         }
@@ -41,9 +51,13 @@ public:
     {
         // Apply a small linear velocity to the model.
         //this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
-        this->gasPedalJoint->SetForce(0, 500000);
         this->gasPedalJoint->SetForce(0, 0);
         this->gasPedalJoint->SetForce(0, 0);
+        this->gasPedalJoint->SetForce(0, 0);
+        this->flWheelJoint->SetForce(0, -10);
+        this->frWheelJoint->SetForce(0, -10);
+        this->blWheelJoint->SetForce(0, -10);
+        this->brWheelJoint->SetForce(0, -10);
     }
 
     // Pointer to the model
@@ -52,6 +66,10 @@ private:
     physics::JointPtr gasPedalJoint;
     physics::JointPtr handBrakeJoint;
     physics::JointPtr brakeJoint;
+    physics::JointPtr flWheelJoint;
+    physics::JointPtr frWheelJoint;
+    physics::JointPtr blWheelJoint;
+    physics::JointPtr brWheelJoint;
 
     // Pointer to the update event connection
 private:
