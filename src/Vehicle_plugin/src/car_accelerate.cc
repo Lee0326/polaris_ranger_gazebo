d13266d6baa39ff2add419cb7f3227e817c36bbc
Lee0326 <lcyfly1@163.com>
2020-03-28 09:47:04 -0400

    add steering joint

--- src/Vehicle_plugin/src/car_accelerate.cc
+++ src/Vehicle_plugin/src/car_accelerate.cc
@@ -20,6 +20,8 @@ public:
         std::string frWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("front_right_wheel");
         std::string blWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("back_left_wheel");
         std::string brWheelJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("back_right_wheel");
+        std::string flSteeringJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("front_left_wheel_steering");
+        std::string frSteeringJointName = this->model->GetName() + "::" + _sdf->Get<std::string>("front_right_wheel_steering");
 
         this->gasPedalJoint = this->model->GetJoint(gasPedalJointName);
         this->handBrakeJoint = this->model->GetJoint(handBrakeJointName);
@@ -28,6 +30,8 @@ public:
         this->frWheelJoint = this->model->GetJoint(frWheelJointName);
         this->blWheelJoint = this->model->GetJoint(blWheelJointName);
         this->brWheelJoint = this->model->GetJoint(brWheelJointName);
+        this->flWheelSteeringJoint = this->model->GetJoint(flSteeringJointName);
+        this->frWheelSteeringJoint = this->model->GetJoint(frSteeringJointName);
 
         if (!this->gasPedalJoint || !this->handBrakeJoint || !this->brakeJoint || !this->flWheelJoint || !this->frWheelJoint || !this->blWheelJoint || !this->brWheelJoint)
         {
@@ -58,6 +62,8 @@ public:
         this->frWheelJoint->SetForce(0, -10);
         this->blWheelJoint->SetForce(0, -10);
         this->brWheelJoint->SetForce(0, -10);
+        this->flWheelSteeringJoint->SetForce(0, 10);
+        this->frWheelSteeringJoint->SetForce(0, 10);
     }
 
     // Pointer to the model
@@ -70,6 +76,8 @@ private:
     physics::JointPtr frWheelJoint;
     physics::JointPtr blWheelJoint;
     physics::JointPtr brWheelJoint;
+    physics::JointPtr flWheelSteeringJoint;
+    physics::JointPtr frWheelSteeringJoint;
 
     // Pointer to the update event connection
 private:
